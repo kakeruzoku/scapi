524110a4f853ce014195262e2785c7f966a65458
かける族 <kakeruzoku@gmail.com>
2025-07-21 13:56:56 +0900

    2.2.0

--- scapi/others/common.py
+++ scapi/others/common.py
@@ -16,7 +16,7 @@ _T = TypeVar("_T")
 if TYPE_CHECKING:
     from ..sites import session
 
-__version__ = "2.1.1"
+__version__ = "2.2.0"
 
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
